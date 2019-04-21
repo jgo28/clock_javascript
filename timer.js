5fc5b4a4001812d2710ba8e6c9c7e044e9ee01e0
Mauricio <mvelazquez@zagmail.gonzaga.edu>
2019-04-20 18:15:17 -0700

    more Changes

--- timer.js
+++ timer.js
@@ -1,29 +1,40 @@
-"use strict";
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-​
-// Update the count down every 1 second
-var x = setInterval(function() {
-​
-  // Get todays date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+function start() {
+    var countdownTimer = setInterval('timer()', 1000);
   }
-}, 1000);
+  
+  function timer() {
+    var d = parseInt(document.getElementById("days").value, 0);
+    var h = parseInt(document.getElementById("hours").value, 0);
+    var m = parseInt(document.getElementById("minutes").value, 0);
+    var s = parseInt(document.getElementById("seconds").value, 0);
+  
+    var now = new Date();
+    var date = now.getTime();
+  
+    addDay = now.setDate(now.getDate() + d);
+    addHour = now.setHours(now.getHours() + h);
+    addMinute = now.setMinutes(now.getMinutes() + m);
+    addSecond = now.setSeconds(now.getSeconds() + s);
+  
+    var then = new Date(addHour + addMinute + addSecond);
+  
+    if(d > 0 || h > 0 || m > 0 || s > 0){
+      var final = then - date;
+      var dd = Math.floor(final/ (1000 * 60 * 60 * 24));
+      var hh = Math.floor((final / (1000 * 60 * 60)) % 24);
+      var mm = Math.floor((final / 1000 / 60) % 60);
+      var ss = Math.floor((final / 1000) % 60);
+  
+      document.getElementById("display").innerHTML = "Time Remaining: " + dd + "D     " + hh + "H " + mm + "M " + ss + "S";
+  
+      document.getElementById("message").innerHTML = then;
+  
+      if (final < 0) {
+        clearInterval(countdownTimer);
+        document.getElementById("message").innerHTML = "Expired";
+      }
+    }else{
+      document.getElementById("display").innerHTML = " ";
+      document.getElementById("message").innerHTML = "Countdown Not Started";
+    }
+  }

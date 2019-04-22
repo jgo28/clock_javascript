e4b0c04d3105fae2215b3f0c92054a2b3403cdd1
Mauricio <mvelazquez@zagmail.gonzaga.edu>
2019-04-22 14:57:08 -0700

    changes

--- timer.js
+++ timer.js
@@ -1,64 +1,95 @@
 "use strict";
-//set minutes 
-var mins = 2; 
-  
-//calculate the seconds 
-var secs = mins * 60; 
 
-//countdown function is evoked when page is loaded 
-function countdown() { 
-    setTimeout('Decrement()', 60); 
-} 
+var hours = document.getElementById("hour").value;
+var minutes = document.getElementById("min").value;
+var seconds = document.getElementById("sec").value;
+document.getElementById("time_left").innerText = "00:00:00";    // displays time on web page
 
-//Decrement function decrement the value. 
-function Decrement() { 
-    if (document.getElementById) { 
-        minutes = document.getElementById("minutes"); 
-        seconds = document.getElementById("seconds"); 
-
-        //if less than a minute remaining 
-        //Display only seconds value. 
-        if (seconds < 59) { 
-            seconds.value = secs; 
-        } 
-
-        //Display both minutes and seconds 
-        //getminutes and getseconds is used to 
-        //get minutes and seconds 
-        else { 
-            minutes.value = getminutes(); 
-            seconds.value = getseconds(); 
-        } 
-        //when less than a minute remaining 
-        //colour of the minutes and seconds 
-        //changes to red 
-        if (mins < 1) { 
-            minutes.style.color = "red"; 
-            seconds.style.color = "red"; 
-        } 
-        //if seconds becomes zero, 
-        //then page alert time up 
-        if (mins < 0) { 
-            alert('time up'); 
-            minutes.value = 0; 
-            seconds.value = 0; 
-        } 
-        //if seconds > 0 then seconds is decremented 
-        else { 
-            secs--; 
-            setTimeout('Decrement()', 1000); 
-        } 
-    } 
-} 
-
-function getminutes() { 
-    //minutes is seconds divided by 60, rounded down 
-    mins = Math.floor(secs / 60); 
-    return mins; 
-} 
-
-function getseconds() { 
-    //take minutes remaining (as seconds) away  
-    //from total seconds remaining 
-    return secs - Math.round(mins * 60);
+start.onclick = function(){
+  time_left =  hours + ":" + minutes + ":" + seconds;  
+  document.getElementById("time_left").innerHTML = time_left;    // displays time on web page
 }
+
+
+function getTime() {
+  var hours = document.getElementById("hour").value;
+  var minutes = document.getElementById("min").value;
+  var seconds = document.getElementById("sec").value;
+  document.getElementById("time_left").innerText = "00:00:00";
+  // time_left =  hours + ":" + minutes + ":" + seconds;  
+  // document.getElementById("time_left").innerText = time_left;
+  setTimeout(getTime, 100); // update the clock
+
+}
+
+
+
+getTime();
+
+
+
+
+// "use strict";
+// //set minutes 
+// var mins = 2; 
+  
+// //calculate the seconds 
+// var secs = mins * 60; 
+
+// //countdown function is evoked when page is loaded 
+// function countdown() { 
+//     setTimeout('Decrement()', 60); 
+// } 
+
+// //Decrement function decrement the value. 
+// function Decrement() { 
+//     if (document.getElementById) { 
+//         minutes = document.getElementById("minutes"); 
+//         seconds = document.getElementById("seconds"); 
+
+//         //if less than a minute remaining 
+//         //Display only seconds value. 
+//         if (seconds < 59) { 
+//             seconds.value = secs; 
+//         } 
+
+//         //Display both minutes and seconds 
+//         //getminutes and getseconds is used to 
+//         //get minutes and seconds 
+//         else { 
+//             minutes.value = getminutes(); 
+//             seconds.value = getseconds(); 
+//         } 
+//         //when less than a minute remaining 
+//         //colour of the minutes and seconds 
+//         //changes to red 
+//         if (mins < 1) { 
+//             minutes.style.color = "red"; 
+//             seconds.style.color = "red"; 
+//         } 
+//         //if seconds becomes zero, 
+//         //then page alert time up 
+//         if (mins < 0) { 
+//             alert('time up'); 
+//             minutes.value = 0; 
+//             seconds.value = 0; 
+//         } 
+//         //if seconds > 0 then seconds is decremented 
+//         else { 
+//             secs--; 
+//             setTimeout('Decrement()', 1000); 
+//         } 
+//     } 
+// } 
+
+// function getminutes() { 
+//     //minutes is seconds divided by 60, rounded down 
+//     mins = Math.floor(secs / 60); 
+//     return mins; 
+// } 
+
+// function getseconds() { 
+//     //take minutes remaining (as seconds) away  
+//     //from total seconds remaining 
+//     return secs - Math.round(mins * 60);
+// }

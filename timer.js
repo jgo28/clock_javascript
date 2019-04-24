6cb9dd75288d984d06987dd71270e4049e7f3edd
Mauricio <mvelazquez@zagmail.gonzaga.edu>
2019-04-24 16:07:17 -0700

    Changes

--- timer.js
+++ timer.js
@@ -1,127 +1,94 @@
- "use strict";
+"use strict";
  
 // Hides timer UI
 function hideTimer(mode) {
   var timer = document.getElementById("timer");
   var buttons = document.getElementById("timer_buttons");
+  var clock = document.getElementById("t_clock");
   if (mode === 1){// reveal ui elements
       timer.style.display = "block";
       buttons.style.display = "block";
+      clock.style.display = "block";
   }else{    // hide ui elements
       timer.style.display = "none";
       buttons.style.display = "none";
+      clock.style.display = "none";
   }
 }
 
+//Reset button
 function reset(){
   var hours = document.getElementById("hours");
   var minutes = document.getElementById("minutes");
   var seconds = document.getElementById("seconds");
+  var timer = document.getElementById("timer");
+  var clock = document.getElementById("t_clock");
+  clock.style.display = "none";
+  timer.style.display = "block";
   hours.value = "";
   minutes.value = "";
   seconds.value = "";
+}
+
+//Start Button
+function start(){
+  var hours = document.getElementById("hours").value;
+  var minutes = document.getElementById("minutes").value;
+  var seconds = document.getElementById("seconds").value;
+
+  var timer = document.getElementById("timer");
+  var t_clock = document.getElementById("t_clock");
+  t_clock.style.display = "block";
+  timer.style.display = "none";
+
+  var total_time_seconds = (hours * 360) + (minutes * 60) + seconds
+  
+  var display = document.querySelector('#time');
+
+  document.getElementById("t_clock").textContent =  startTimer(total_time_seconds, display);
 
 }
 
-// function reset(){
-//   alert("this")
-// }
-
+//Handle buttons
 document.getElementById("reset").onclick = reset;
+document.getElementById("start").onclick = start;
 
 
+function startTimer(duration, display) {
+  var start = Date.now(),
+      diff,
+      hours,
+      minutes,
+      seconds;
+  function timer() {
+      // get the number of seconds that have elapsed since 
+      // startTimer() was called
+      diff = duration - (((Date.now() - start) / 1000) | 0);
 
+      // does the same job as parseInt truncates the float
+      seconds = Math.floor( (diff) % 60 );
+      minutes = Math.floor( (diff/60) % 60 );
+      hours = Math.floor((diff/(60*60))) % 24;
 
-// var hours = document.getElementById("hour").value;
-// var minutes = document.getElementById("min").value;
-// var seconds = document.getElementById("sec").value;
-// document.getElementById("time_left").innerText = "00:00:00";    // displays time on web page
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-// start.onclick = function(){
-//   time_left =  hours + ":" + minutes + ":" + seconds;  
-//   document.getElementById("time_left").innerHTML = time_left;    // displays time on web page
-// }
+      display.textContent = hours + ":" + minutes + ":" + seconds; 
 
+      if (diff <= 0) {
+          // add one second so that the count down starts at the full duration
+          // example 05:00 not 04:59
+          start = Date.now() + 1000;
+      }
+  };
+  // we don't want to wait a full second before the timer starts
+  timer();
+  setInterval(timer, 100);
+}
 
-// function getTime() {
-//   var hours = document.getElementById("hour").value;
-//   var minutes = document.getElementById("min").value;
-//   var seconds = document.getElementById("sec").value;
-//   document.getElementById("time_left").innerText = "00:00:00";
-//   // time_left =  hours + ":" + minutes + ":" + seconds;  
-//   // document.getElementById("time_left").innerText = time_left;
-//   setTimeout(getTime, 100); // update the clock
-
-// }
-
-
-
-// getTime();
-
-
-
-
-// "use strict";
-// //set minutes 
-// var mins = 2; 
-  
-// //calculate the seconds 
-// var secs = mins * 60; 
-
-// //countdown function is evoked when page is loaded 
-// function countdown() { 
-//     setTimeout('Decrement()', 60); 
-// } 
-
-// //Decrement function decrement the value. 
-// function Decrement() { 
-//     if (document.getElementById) { 
-//         minutes = document.getElementById("minutes"); 
-//         seconds = document.getElementById("seconds"); 
-
-//         //if less than a minute remaining 
-//         //Display only seconds value. 
-//         if (seconds < 59) { 
-//             seconds.value = secs; 
-//         } 
-
-//         //Display both minutes and seconds 
-//         //getminutes and getseconds is used to 
-//         //get minutes and seconds 
-//         else { 
-//             minutes.value = getminutes(); 
-//             seconds.value = getseconds(); 
-//         } 
-//         //when less than a minute remaining 
-//         //colour of the minutes and seconds 
-//         //changes to red 
-//         if (mins < 1) { 
-//             minutes.style.color = "red"; 
-//             seconds.style.color = "red"; 
-//         } 
-//         //if seconds becomes zero, 
-//         //then page alert time up 
-//         if (mins < 0) { 
-//             alert('time up'); 
-//             minutes.value = 0; 
-//             seconds.value = 0; 
-//         } 
-//         //if seconds > 0 then seconds is decremented 
-//         else { 
-//             secs--; 
-//             setTimeout('Decrement()', 1000); 
-//         } 
-//     } 
-// } 
-
-// function getminutes() { 
-//     //minutes is seconds divided by 60, rounded down 
-//     mins = Math.floor(secs / 60); 
-//     return mins; 
-// } 
-
-// function getseconds() { 
-//     //take minutes remaining (as seconds) away  
-//     //from total seconds remaining 
-//     return secs - Math.round(mins * 60);
-// }
+// window.onload = function () {
+//   var fiveMinutes = 60 * 5,
+//       display = document.querySelector('#time');
+//   startTimer(fiveMinutes, display);
+// };

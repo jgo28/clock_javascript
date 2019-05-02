2b04253d583998abd060047341d2bb31db2cd4c1
Mauricio <mvelazquez@zagmail.gonzaga.edu>
2019-05-01 19:44:19 -0700

    Everything but reset

--- timer.js
+++ timer.js
@@ -9,14 +9,26 @@ function hideTimer(mode) {
   var timer = document.getElementById("timer");
   var buttons = document.getElementById("timer_buttons");
   var clock = document.getElementById("t_clock");
+  var time = document.getElementById("time");
+
   if (mode === 1){// reveal ui elements
       timer.style.display = "block";
       buttons.style.display = "block";
       clock.style.display = "block";
+      time.style.display = "block";
+      if (document.getElementById("start").disabled == true){
+        timer.style.display = "none";
+      }
+
+
   }else{    // hide ui elements
       timer.style.display = "none";
       buttons.style.display = "none";
       clock.style.display = "none";
+      time.style.display = "none";
+      if (document.getElementById("start").disabled == true){
+        timer.style.display = "none";
+            }
   }
 }
 
@@ -40,7 +52,6 @@ function reset(){
   seconds.value = "";
 }
 
-
 var display = document.querySelector('#time');
 //Start Button
 function start(){
@@ -52,7 +63,7 @@ function start(){
 
   var timer = document.getElementById("timer");
   var t_clock = document.getElementById("t_clock");
-  t_clock.style.display = "block";
+  t_clock.style.display = "none";
   timer.style.display = "none";
 
   var total_time_seconds = (hours * 360 * 10) + (minutes * 60) + seconds
@@ -64,7 +75,6 @@ var interval;
 var paused = false;
 var timeLeft;
 function pause(){
-  console.log(diff)
   if(!paused){
     paused = true;
     clearInterval(interval);
@@ -76,8 +86,6 @@ function pause(){
     document.getElementById("pause").textContent = "Pause";
     document.getElementById("t_clock").textContent =  startTimer(timeLeft, display);
   }
-  console.log(paused);
-
 }
 
 //Handle buttons
@@ -98,7 +106,6 @@ function startTimer(duration, display) {
       // startTimer() was called
       diff = duration - (((Date.now() - start) / 1000) | 0);
 
-
       // does the same job as parseInt truncates the float
       hours = Math.floor((diff/(60*60)));
       minutes = Math.floor( (diff/60) % 60 );
@@ -110,13 +117,12 @@ function startTimer(duration, display) {
 
       display.textContent = hours + ":" + minutes + ":" + seconds; 
 
-      if (diff <= 0) {
-          // add one second so that the count down starts at the full duration
-          // example 05:00 not 04:59
-          start = Date.now() + 1000;
+      if (diff < 0){
+        reset();
+        clearInterval(interval);
+        alert("Times is up")
       }
   };
-  // we don't want to wait a full second before the timer starts
   timer();
-  interval = setInterval(timer, 100);
+  interval = setInterval(timer, 1000);
 }
